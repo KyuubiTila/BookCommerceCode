bc149c498ce564cf5b5ac6e0f57ba3d698828f3d
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-05-31 17:11:39 +0100

    drop down for account profile

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,55 +1,61 @@
-import Logo from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
 import { Search } from '../Sections/Search';
+import { DropdownLoggedOut, DropdownLoggedIn } from '../index';
 
 export const Header = () => {
-  const [light, setLight] = useState(
-    JSON.parse(localStorage.getItem('light')) || false
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem('darkMode')) || false
   );
-  const [search, setSearch] = useState(false);
+  const [searchSection, setSearchSection] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem('light', JSON.stringify(light));
-    light
-      ? document.documentElement.classList.remove('dark')
-      : document.documentElement.classList.add('dark');
-  }, [light]);
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+      <nav className="bg-white dark:bg-gray-900">
+        <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
           <Link to="/" className="flex items-center">
-            <img src={Logo} className="h-8 mr-3" alt="CodeBook Logo" />
+            <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               CodeBook
             </span>
           </Link>
-          <div className="flex item-center">
-            <span onClick={() => setLight(!light)}>
-              {light ? (
-                <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi bi-moon"></span>
-              ) : (
-                <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi bi-moon-fill"></span>
-              )}
-            </span>
+          <div className="flex items-center relative">
             <span
-              onClick={() => setSearch(!search)}
+              onClick={() => setDarkMode(!darkMode)}
+              className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"
+            ></span>
+            <span
+              onClick={() => setSearchSection(!searchSection)}
               className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"
             ></span>
-            <Link to="/products" className="text-gray-700 dark:text-white mr-5">
-              <span className="relative cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-cart">
+            <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
+              <span className="text-2xl bi bi-cart-fill relative">
                 <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">
                   0
                 </span>
               </span>
             </Link>
-            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-person"></span>
+            <span
+              onClick={() => setDropdown(!dropdown)}
+              className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"
+            ></span>
+            {dropdown && <DropdownLoggedOut />}
           </div>
         </div>
       </nav>
-      {search && <Search setSearch={setSearch} search={search} />}
+      {searchSection && <Search setSearchSection={setSearchSection} />}
     </header>
   );
 };
